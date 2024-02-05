8bc392caeddb4efb992a30eb7db83ce2e79ec0e5
GrahamSH <grahamshllk@gmail.com>
2024-02-05 18:27:50 +0000

    imagen

--- imagine/index.js
+++ imagine/index.js
@@ -1,32 +1,40 @@
 import { createApp, eventHandler, toNodeListener, createRouter } from "h3";
 import { listen } from "listhen";
-import nodeHtmlToImage from "node-html-to-image";
+//import nodeHtmlToImage from "node-html-to-image";
+import puppeteer from "puppeteer";
+import rules from "../src/lib/2024.js";
+const browser = await puppeteer.launch();
+
+// Create a new page
+const page = await browser.newPage();
+
+const takeScreenshot = async (html) => {
+  await page.setContent(html, { waitUntil:'domcontentloaded' });
+  const element = await page.$("body");
+  const buffer = await element.screenshot({
+   /* type: screenshot.type,
+    omitBackground: screenshot.transparent,
+    encoding: screenshot.encoding,
+  quality: screenshot.quality,*/
+  optimizeForSpeed: true  });
+
+  return buffer;
+};
 const app = createApp();
-const router = createRouter()
-
+const router = createRouter();
 router.get(
-  "/rule/:rule",
+  "/rule/:rule/image.png",
   eventHandler(async (event) => {
-
-    const res = await fetch(`https://frctools.com/api/rule?query=${event.context.params.rule}&year=2024`);
-    const html = (await res.json()).text
-    const img = await nodeHtmlToImage({
-      output: "./image.png",
-      html: `<html>
-          <head>
-            <style>
-              /*body {
-                width: 2480px;
-                height: 3508px;
-              }*/
-            </style>
-          </head>
-          <body>${html}</body>
-        </html>
-        `,
-    });
+    let html;
+    try {html = rules[event.context.params.rule.toUpperCase()].text;
+    } catch {
+        return "error 404"
+    }
+    //  const res = await fetch(`https://frctools.com/api/rule?query=${event.context.params.rule}&year=2024`);
+    //const html = (await res.json()).text
+    const img = await takeScreenshot(html)
     return img;
   })
 );
-app.use(router)
+app.use(router);
 listen(toNodeListener(app));
