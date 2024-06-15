ef6baaeb3d1fdb76de89286d96d586ac29c867f7
GrahamSH <grahamshllk@gmail.com>
2024-06-15 14:59:38 +0000

    add sentry

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,4 +1,14 @@
-export async function handle({ event, resolve }) {
+import {sequence} from "@sveltejs/kit/hooks";
+import * as Sentry from "@sentry/sveltekit";
+
+Sentry.init({
+    dsn: "https://b3581cc518dfa0ad74ad8bd851c28c1d@o1005541.ingest.us.sentry.io/4507436532957184",
+    tracesSampleRate: 1
+})
+
+export const handleError = Sentry.handleErrorWithSentry();
+
+export const handle = sequence(Sentry.sentryHandle(), async function _handle({ event, resolve }) {
   if (event.url.pathname == "/") {
     return Response.redirect(
       "https://" + event.url.hostname + "/" + new Date().getFullYear(),
@@ -13,4 +23,4 @@ export async function handle({ event, resolve }) {
   // If no redirect is necessary, continue with the normal resolve process
   const response = await resolve(event);
   return response;
-}
+});
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
+import { sentrySvelteKit } from "@sentry/sveltekit";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [sveltekit()],
+  plugins: [sentrySvelteKit({
+    sourceMapsUploadOptions: {
+      org: "ejenda",
+      project: "rules-search"
+    }
+  }), sveltekit()],
 
 });
