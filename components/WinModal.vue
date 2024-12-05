2fab870c3c2711e0e15c3cab8462309c1b1ff218
GrahamSH <grahamshllk@gmail.com>
2024-12-05 16:29:31 +0000

    add bingo (shh)

---
+++ components/WinModal.vue
@@ -0,0 +1,17 @@
+<template>
+  <UModal>
+    <template #body>
+      Congratulations for winning 2025 bingo! Make sure to check out our <a class="bold text-blue-400" @click="navigate" href="#">rule search</a>!
+    </template>
+    <template #footer>
+      <UButton color="neutral" label="Keep playing" @click="modal.close()" />
+    </template>
+  </UModal>
+</template>
+<script setup>
+const modal = useModal();
+const navigate = async () => {
+   modal.close();
+   await navigateTo("/");
+}
+</script>
---
+++ pages/bingo.vue
@@ -0,0 +1,154 @@
+<template>
+  <div>
+    <Nav hide-year />
+    <UContainer class="mt-4">
+      <div class="flex flex-wrap gap-2 overflow-x-auto p-2">
+        <div
+          v-for="(row, i) in rows"
+          :key="i + 'row'"
+          class="flex flex-row gap-2"
+        >
+          <UCard
+            class="card size-48 flex justify-center items-center cursor-pointer rounded-md"
+            v-for="card of row"
+            :key="card"
+            @click="toggle(card)"
+            :class="
+              isSelected(card) ? 'border-green-400 border bg-green-100' : ''
+            "
+          >
+            <span>{{ card }}</span>
+          </UCard>
+        </div>
+        {{ hasWon }}
+      </div>
+      <div class="italic flex w-full justify-center">
+        a frctools project - check out our rules search at
+        <NuxtLink to="/" class="bold text-blue-500 ml-1">
+          https://frctools.com</NuxtLink
+        >
+      </div>
+    </UContainer>
+  </div>
+</template>
+
+<script setup>
+import WinModal from "~/components/WinModal.vue"
+const BINGO_CARDS = [
+  "Dean Kamen goes on a random STEM tangent",
+  "Water Game Teased",
+  "Dean Kamen repeatedly hits table with hand",
+  "We’re about to reveal it but first",
+  "Dozer in the game animation",
+  "Space Force commercial",
+  "Graphics that have nothing to do with the theme of the game",
+  "Some pun on the name Reefscape is made",
+  "Technical difficulties",
+  "HAAS advertisement",
+  'Dean Kamen is "excited to see" something happen this year',
+  "will.i.am shows up",
+  "Game has seemingly no connection to the name Reefscape",
+  "Game Animation shows illegal robots",
+  "Spot light on a FIRST employee",
+  "Shout out to FLL and FTC",
+  "Shooting game",
+  "Pick and place game",
+  "Game has no endgame",
+  "Game has crowd interaction",
+  "Game is great for swerve",
+  "The game is played with balls",
+  "PVC pipe is a game piece",
+  'Main game is on a "Reef"',
+  "FIRST opportunity for everyone",
+  "FREE SPACE",
+  "Game code leaked early",
+  "Hardest fun you can have",
+  "We are so excited",
+  "Student interviews",
+  "Premiere chat goes quite for Woodie Flowers tribute",
+  "Woodie flowers tribute",
+  "Random sponsor commercial",
+  "Game reveal video features dozer abuse",
+  "Robots building children",
+  "It's a terrain game",
+];
+const { data: pickedCards } = await useAsyncData("pickedCards", () => {
+  const pickedCards = [];
+  for (let i = 0; i < 25; i++) {
+    let card = BINGO_CARDS[Math.floor(Math.random() * BINGO_CARDS.length)];
+    while (pickedCards.includes(card)) {
+      card = BINGO_CARDS[Math.floor(Math.random() * BINGO_CARDS.length)];
+    }
+    pickedCards.push(card);
+  }
+  return pickedCards;
+});
+
+const selectedCards = ref([]);
+const toggle = (card) => {
+  if (!selectedCards.value.includes(card)) {
+    selectedCards.value.push(card);
+  } else {
+    selectedCards.value = selectedCards.value.filter((c) => c !== card);
+  }
+};
+const isSelected = (card) => {
+  return selectedCards.value.includes(card);
+};
+const rows = computed(() => {
+  // make rows from pickedCards
+  return pickedCards.value.reduce((rows, card, index) => {
+    if (index % 5 === 0) {
+      rows.push([card]);
+    } else {
+      rows[rows.length - 1].push(card);
+    }
+    return rows;
+  }, []);
+});
+
+const hasWon = computed(() => {
+  /* if (i == 0) {
+      for (const item of row) {
+        // traverse through column
+        for (let j = 0; j < rows.value.length; j++) {
+          if (!rows.value[j].includes(item)) {
+            break;
+          }
+        }
+        return true;
+      }
+    }*/
+  const rowWins = rows.value.map((row) => {
+    return !row.some((x) => !isSelected(x));
+  });
+  let columnWins = [0, 1, 2, 3, 4].map((column) => {
+    return rows.value.every((row) => isSelected(row[column]));
+  });
+
+  const leftDiagonalWin = (() => {
+    for (let i = 0; i < rows.value.length; i++) {
+      if (!isSelected(rows.value[i][i])) {
+        return false;
+      }
+    }
+    return true;
+  })();
+  const rightDiagonalWin = (() => {
+    for (let i = 0; i < rows.value.length; i++) {
+      if (!isSelected(rows.value[i][rows.value.length - 1 - i])) {
+        return false;
+      }
+    }
+    return true;
+  })();
+  return rowWins.some((x) => x) || columnWins.some((x) => x) || leftDiagonalWin || rightDiagonalWin;
+});
+watch(hasWon, () => {
+   const modal = useModal();
+   if (hasWon.value) {
+      modal.open(WinModal, {title: "You win!"})
+
+   }
+})
+</script>
