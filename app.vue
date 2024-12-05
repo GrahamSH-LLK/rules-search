5e13eb8680b70e8f199d077e7413d386d2c4f2c5
GrahamSH <grahamshllk@gmail.com>
2024-12-05 18:11:54 +0000

    add

--- app.vue
+++ app.vue
@@ -4,3 +4,34 @@
     <NuxtPage />
   </UApp>
 </template>
+<script setup>
+onMounted(() => {
+  if (
+    window &&
+    !localStorage.getItem("bingo-25-shown") &&
+    new Date().getTime() > 1735102800000
+  ) {
+    // christmas 2024
+    const toast = useToast().add({
+      title: "Kickoff Bingo",
+      description: `Check out kickoff bingo!`,
+      icon: "i-lucide-calendar-days",
+      duration: 0,
+      actions: [
+        {
+          icon: "i-lucide-square-arrow-out-up-right",
+          label: "Go to bingo",
+          color: "neutral",
+          variant: "outline",
+          onClick: async (e) => {
+            e?.stopPropagation();
+            localStorage.setItem("bingo-25-shown", true);
+            await navigateTo("/bingo");
+
+          },
+        },
+      ],
+    });
+  }
+});
+</script>
