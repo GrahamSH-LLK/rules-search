5ae63dbe87ba71b018034fbc3c6b22249c773f62
GrahamSH <grahamshllk@gmail.com>
2025-01-04 23:32:42 +0000

    add 2025

--- composables/index.ts
+++ composables/index.ts
@@ -4,18 +4,18 @@
 */
 export const useYearsNav = () => {
   return [
-    /*{
+    {
       label: "2025 - Reefscape",
       value: "2025",
       emoji: "🌊",
-    },*/
+    },
+    { label: "2025 - INTO THE DEEP", value: "2025-ftc", emoji: "🪸" },
 
     {
       label: "2024 - Crescendo",
       value: "2024",
       emoji: "🎶",
     },
-    { label: "2025 - INTO THE DEEP", value: "2025-ftc", emoji: "🪸" },
     {
       label: "2023 - Charged Up",
       value: "2023",
@@ -25,10 +25,10 @@ export const useYearsNav = () => {
       label: "2022 - Rapid React",
       value: "2022",
       emoji: "✈️",
-    }
+    },
   ];
 };
 
 export const useYears = () => {
-  return useYearsNav().map(year => year.value);
+  return useYearsNav().map((year) => year.value);
 };
