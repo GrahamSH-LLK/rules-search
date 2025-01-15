32d212c5418657a7514f72018b744d94d1e3ca4c
GrahamSH <grahamshllk@gmail.com>
2025-01-15 13:52:14 +0000

    add error handling

--- components/RenderHtmlTooltip.vue
+++ components/RenderHtmlTooltip.vue
@@ -6,24 +6,30 @@ export default {
   async setup(props) {
     const Tooltip = resolveComponent("Tooltip");
     let html = `<div class="prose max-w-full dark:prose-invert overflow-x-auto px-4">${props.html}</div>`;
+    let doc;
+    try {
+      doc = parse(html);
+    } catch (e) {
+      console.error(e);
+      return () => { 
+         return h("p", "Failed to parse HTML");
+      };
+    }
 
-    const doc = parse(html);
-
-    function processNode(node) {
+    function processNode(node: Node): any {
       if (node.nodeType === 3) {
         const text = node.textContent;
         return text ? () => text : null;
       }
 
-      if (node.nodeType === 1) {
+      if (node.nodeType == 1 || node instanceof Element) {
         const tag = node.tagName.toLowerCase();
 
-        const props = {};
-      
+        const props: Record<string, any> = {};
+
         Array.from(node.attributes).forEach((attr) => {
           props[attr.name] = attr.value;
         });
-        
 
         const children = Array.from(node.childNodes)
           .map(processNode)
@@ -32,7 +38,9 @@ export default {
           h(
             tag == "tooltip" ? Tooltip : tag,
             props,
-            tag == 'tooltip' ? {default:()=> node.textContent} :children.map((child) => child())
+            tag == "tooltip"
+              ? { default: () => node.textContent }
+              : children.map((child) => child())
           );
       }
 
